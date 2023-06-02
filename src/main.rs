6d583ac5a404b9c230a9656a081771b8410828fb
ambiso <ambisotopy@gmail.com>
2023-06-02 14:57:59 +0200

    Fmt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use der::{Encode, AnyRef};
-use der::asn1::{UintRef, BitString};
+use der::asn1::{BitString, UintRef};
+use der::{AnyRef, Encode};
 use rsa::pkcs1::RsaPublicKey as RsaPublicKeyPkcs1;
-use rsa::pkcs8::SubjectPublicKeyInfo;
 use rsa::pkcs1v15::SigningKey;
+use rsa::pkcs8::SubjectPublicKeyInfo;
 use rsa::sha2::Sha256;
 use rsa::traits::PublicKeyParts;
 use rsa::{RsaPrivateKey, RsaPublicKey};
-use x509_cert::spki::{AssociatedAlgorithmIdentifier};
+use x509_cert::spki::AssociatedAlgorithmIdentifier;
 
 fn main() {
     let mut rng = rand::thread_rng();
@@ -17,10 +17,11 @@ fn main() {
     let pub_key_der = RsaPublicKeyPkcs1 {
         public_exponent: UintRef::new(pub_key.e().to_bytes_be().as_slice()).unwrap(),
         modulus: UintRef::new(pub_key.n().to_bytes_be().as_slice()).unwrap(),
-    }.to_der().unwrap();
+    }
+    .to_der()
+    .unwrap();
 
-    let subject_public_key_info =
-    SubjectPublicKeyInfo::<AnyRef<'static>, BitString> {
+    let subject_public_key_info = SubjectPublicKeyInfo::<AnyRef<'static>, BitString> {
         algorithm: SigningKey::<Sha256>::ALGORITHM_IDENTIFIER,
         subject_public_key: BitString::new(0, pub_key_der).unwrap(),
     };
