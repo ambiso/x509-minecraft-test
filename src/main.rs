9d766ddd6c2a2070eb65e6ed332533e75f1f2c35
ambiso <ambisotopy@gmail.com>
2023-06-02 17:53:17 +0200

    Minify

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-use der::asn1::{BitString, UintRef};
-use der::{AnyRef, Encode};
-use rsa::pkcs1::RsaPublicKey as RsaPublicKeyPkcs1;
-use rsa::pkcs1v15::SigningKey;
-use rsa::pkcs8::SubjectPublicKeyInfo;
-use rsa::sha2::Sha256;
-use rsa::traits::PublicKeyParts;
+use pkcs8::EncodePublicKey;
 use rsa::{RsaPrivateKey, RsaPublicKey};
-use x509_cert::spki::AssociatedAlgorithmIdentifier;
 
 fn main() {
     let mut rng = rand::thread_rng();
@@ -14,19 +7,7 @@ fn main() {
     let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
     let pub_key = RsaPublicKey::from(&priv_key);
 
-    let pub_key_der = RsaPublicKeyPkcs1 {
-        public_exponent: UintRef::new(pub_key.e().to_bytes_be().as_slice()).unwrap(),
-        modulus: UintRef::new(pub_key.n().to_bytes_be().as_slice()).unwrap(),
-    }
-    .to_der()
-    .unwrap();
-
-    let subject_public_key_info = SubjectPublicKeyInfo::<AnyRef<'static>, BitString> {
-        algorithm: SigningKey::<Sha256>::ALGORITHM_IDENTIFIER,
-        subject_public_key: BitString::new(0, pub_key_der).unwrap(),
-    };
-
-    for (i, b) in subject_public_key_info.to_der().unwrap().iter().enumerate() {
+    for (i, b) in pub_key.to_public_key_der().unwrap().as_bytes().iter().enumerate() {
         if i > 0 && i % 40 == 0 {
             println!("");
         }
